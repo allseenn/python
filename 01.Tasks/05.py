72e9ff8437c2e988f40df45fa3da45e98a58b9db
allseen <allseen@live.ru>
2022-09-23 22:02:50 +0300

    Some changes

--- 01.Tasks/05.py
+++ 01.Tasks/05.py
@@ -4,7 +4,7 @@
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 from math import sqrt
-print("To find distance between 2 point\n Enter their coordinates (x; y)\n Two numbers inline seperated by comma")
+print("To find distance between 2 point\nEnter their coordinates (x; y)\nTwo numbers inline seperated by comma")
 a = [float(a) for a in input("Enter coordinates for A: ").split(sep=",")]
 b = [float(b) for b in input("Enter coordinates for B: ").split(sep=",")]
 print("%.2f" % (sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)))

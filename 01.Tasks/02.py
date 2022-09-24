5f292f621ede4a8d12d057cd01a4689ecbee591a
allseen <allseen@live.ru>
2022-09-24 23:47:20 +0300

    02 task input change

--- 01.Tasks/02.py
+++ 01.Tasks/02.py
@@ -1,9 +1,9 @@
 # 2. Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
 # для всех значений предикат.
 print("Lets check for truth ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z")
-x = int(input("Give X: "))
-y = int(input("Give Y: "))
-z = int(input("Give Z: "))
+x = input("Give X: ")
+y = input("Give Y: ")
+z = input("Give Z: ")
 if not(x or y or z) == (not x and not y and not z):
     print(True)
 else:

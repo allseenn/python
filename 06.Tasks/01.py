e278f4d3ee1310a2bdae7b765b40d8e3109ab675
allseen <allseen@live.ru>
2022-10-11 21:36:21 +0300

    Changes

--- 06.Tasks/01.py
+++ 06.Tasks/01.py
@@ -2,15 +2,8 @@
 import random
 # generate random words from syllables
 syllables = ["ма", "за", "ба", "ка", "ша", "бв"]
-count = 0
-text = []
-while count < random.randint(14,16):
-    text.append("".join(random.sample(syllables, 2)))
-    count += 1
+text = list(map(lambda x: "".join(random.sample(syllables,2)), range(random.randint(14,16))))
 print(f' TEXT : {" ".join(text)}')
 # search for syllable "абв" and delete it with whole word
-parsed_text = []
-for x in text:
-    if "абв" not in x:
-        parsed_text.append(x)
+parsed_text = list(x for x in text if "абв" not in x)
 print(f'PARSED: {" ".join(parsed_text)}')
